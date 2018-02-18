ac2196d749f6aa251c94d05e0b862af48e8371ec
tonuselast <insk0007@ya.ru>
2018-02-18 23:42:04 +0300

    7

---
+++ Correttore di postura Tonus Elast_files/translater.js
@@ -0,0 +1,159 @@
+var new_lang =   {
+    
+  
+"lt00": "Correttore di postura Tonus Elast",
+
+"lt0": "CORRETTORE DI POSTURA<span>Tonus Elast</span>",
+
+"lt1": "Correzione rapida della postura",
+
+"lt2": "Liberarsi dei dolori alla schiena",
+
+"lt3": "Rinforzare la spina dorsale e i muscoli della schiena",
+
+"lt4": "Riallineamento della linea delle spalle",
+
+"lt5": "Miglioramento dell’aspetto",
+
+"lt6": "Aumento della sicurezza in sé",
+
+"lt7": "Effetto curativo generico",
+
+"lt8": "-50% solo oggi!",
+
+"lt10": "Prezzo oggi",
+
+"lt12": "Prezzo standard",
+
+"lt13": "Fino alla fine dell’offerta rimangono:",
+
+"lt14": "COMPILATE I CAMPI DEL MODULO E<br>PREMETE «EFFETTUARE L’ORDINE»",
+
+"lt15": "nazione",
+
+"lt16": "NOME E COGNOME",
+
+"lt17": "Telefono",
+
+"lt18": "Volete raddrizzare la postura?<span>Nono sopportate più i dolori alla schiena?</span>",
+
+"lt19": "La postura scorretta — non è solo un problema estetico! Osservate attentamente le due immagini sottostanti!",
+
+"lt20": "<b>Nell’immagine a sinistra</b> è raffigurata una ragazza — <b>tipica portatrice di postura scorretta.</b> Ogni giorno deve combattere <b>con sensazioni di dolore alla schiena.</b> Non sembrerebbe nulla di grave, ma alla lunga non le permettono di vivere una vita normale, la obbligano ad incurvarsi e a non poter effettuare tutti i movimenti.",
+
+"lt21": "Probabilmente per via dei difetti estetici non è molto sicura e soffre di un complesso di <b>scarsa autostima.</b>",
+
+"lt22": "Nel futuro di questa ragazza c’è un mare di complicazioni: <b>Ipertensione, osteocondrosi, dolori cronici, stanchezza cronica, depressione eccetera .</b>",
+
+"lt23": "<b>Nell’immagine a destra</b> vediamo la sua gemella felice <b>con una postura corretta.</b> Lei si gode ogni giornata e nulla la distrae dal realizzarsi al meglio ogni singolo giorno ! I suoi organi interni funzionano correttamente, la circolazione sanguigna è stabile, il cervello funziona in maniera perfetta.",
+
+"lt24": "<b>Quale di queste due vite scegliereste?</b> E’ una domanda retorica! Se siete finiti su questo sito vuol dire che avete portato la vostra attenzione sul fatto che  <b>voi stessi o vostro figlio avete una postura scorretta!</b>",
+
+"lt25": "Guardate questo video:",
+
+"lt26": "Sappiamo come aiutarvi!",
+
+"lt27": "<p>A combattere la postura scorretta vi aiuterà <b>il correttore di postura \"Tonus Elast\"!</b></p> <p>Tonus Elast<b>Corregge la postura</b>e aiuta a prevenire problemi alla spina dorsale. <b>Attenua il dolore</b>in caso di lavori che vi costringono a stare a lungo seduti  blocca l’evoluzione dell’osteocondrosi.</p> &gt;<div class=\"block3-img1\"><div class=\"js__pack pack2\" data-name=\"pack_two.png\" data-path=\"packs\"><img src=\"packs/pack_two_es.png\"></div></div><p>Pesa poco, occupa poco spazio e <b>non si nota sotto i vestiti</b> quindi si può indossare praticamente sempre.</p><p>La costituzione progettata nei dettagli permette<b>un comfort estremo</b>, e i materiali elastici contemporanei<b>sono conformi alle necessità mediche del prodotto.</b> Il design della cinta lo rende molto comodo <b>sia per gli uomini che per le donne</b> — la cinta può essere tranquillamente indossata sotto i vestiti sopra il reggiseno.</p><div class=\"block3-img2\"><div class=\"js__pack pack3\" data-name=\"pack_three.png\" data-path=\"packs\"><img src=\"packs/pack_three_es.png\"></div></div><p>Уil sistema di chiusura permette di regolare il correttore con semplicità.  E il fatto che in caso di sporcizia si può lavare facilmente lo rende un metodo estremamente comodo per curare la salute della propria spina dorsale e dell’organismo in generale.</p>",
+
+"lt28": "Correttore<br>di postura Tonus Elast",
+
+"lt29": "Aiuta a liberarsi dei dolori alla schiena.",
+
+"lt30": "Migliora la postura, utile in caso di incurvamento.",
+
+"lt31": "VA bene sia per gli adulti che per gli adolescenti di qualsiasi sesso.",
+
+"lt32": "Molto comodo da indossare e non si nota sotto i vestiti.",
+
+"lt33": "Il materiale principale del correttore è il cotone.",
+
+"lt34": "Il correttore viene prodotto in due misure: S-M (Misura ai fianchi: 60–81 см)<br>e L–XL (81–102 см).",
+
+"lt35": "Il correttore ha superato ogni certificazione medica e rispetta tutti gli standard internazionali.",
+
+"lt36": "Raccomandato per l’utilizzo curativo dai migliori neurologi italiani",
+
+"lt37": "Le nostre garanzie",
+
+"lt38": "Solo oggi ordinando il correttore di postura potrete riceverlo al 67% del suo prezzo",
+
+"lt39": "Il prodotto è certificato e raccomandato dall’associazione dei neurologi italiani. Ha superato ogni certificazione.",
+
+"lt40": "Consegna rapida via corriere o posta in tutta Italia ed Europa. Non dovrete aspettare a lungo!",
+
+"lt41": "Non accettiamo il pagamento anticipato. Pagherete solo al momento della ricezione dopo averne controllato la qualità.",
+
+"lt42": "Recensioni",
+
+"lt43": "Non sono mai stata una ragazza molto \"gobba\" . Un po’ curva come tutte... Non mi hanno diagnosticato la scoliosi, semplicemente come il 99% degli abitanti del pianeta terra – mi sedevo come capitava, non mi sono mai seduta con la schiena dritta. E poi alla fine ci si stanca di tenere la schiena...",
+
+"lt44": "E dopo che ho indossato il correttore tutto l’inverno mi hanno iniziato a chiedere se fossi una ballerina (!!!) - e tenete conto che non sono affatto magra e tutto sembro tranne che una ballerina.",
+
+"lt45": "Ecco una storia breve con finale felice:) Grazie per un’invenzione così comoda!",
+
+"lt46": "Eleonora Canini",
+
+"lt47": "Sono abituato ad incurvarmi fin dall’infanzia, e la cosa non mi ha mai disturbato, fino a che la ragazza che mi piace mi ha soprannominato Quasimodo. Dopo di questo ho iniziato ad avere complessi e problemi con le ragazze…",
+
+"lt48": "Il correttore di postura mi è stato consigliato da un’amica, mi ha detto di averne ordinato uno per sua figlia, e che dovrei provarlo anche io, dato che c’era un’offerta in corso sul sito.",
+
+"lt49": "E che dire: sono già due mesi che ho le spalle dritte, guardo tutti dall’alto al basso essendo più alto, mi sento molto meglio e ho anche conosciuto una ragazza!:))",
+
+"lt50": "Filippo Lupi",
+
+"lt51": "Non ho mai fatto attenzione alla mia spina dorsale. All’inizio stavo seduta male in università, e sono passati così 6 anni, e poi altri 6 anni così a lavoro con pose improvvisate al computer tutto il tempo!:)",
+
+"lt52": "E beh ora ho 28 anni, e non solo sono curva come un vecchietta, e la schiena inizia a chiedermi sotto forma di dolore di fare qualcosa per rimetterla in salute.",
+
+"lt53": "Non ho tempo di andare in palestra, ma fortunatamente ho trovato questo correttore di postura su internet, l’ho ordinato e lo porto già da un mese. I dolori sono passati già da un paio di settimane, la postura è migliorata di molto. Mi sento molto più attiva!",
+
+"lt54": "Arianna Frisenda",
+
+"lt55": "Offerta! iPhone 5s in regalo!",
+
+"lt56": "Ogni mese mettiamo in palio tra i clienti un Apple iPhone 5s! Ordinate il correttore di postura Tonus Elast e partecipate all’estrazione di fantastici premi!",
+
+"lt57": "Auguri alla vincitrice!",
+
+"lt58": "Serena Giulli<br>22 anni, Firenze",
+
+"lt59": "Effettuare l’ordine sul nostro negozio on line è molto semplice",
+
+"lt60": "Non lavoriamo come gli altri negozi on line, che ci mettono ore a confermare i vostri ordini, аe settimane a «portarvi» l’ordine prendendo i soldi in anticipo. Da noi è tutto più semplice.",
+
+"lt61": "EFFETTUATE L’ORDINE IN <span>5 minuti</span>",
+
+"lt62": "VERIFICHIAMO I DETTAGLI IN <span>5 minuti</span>",
+
+"lt63": "RICEVETE<br>L’ORDINE TRA <span> 1 e 7 giorni</span>",
+
+"lt64": "affrettatevi ad effettuare l’ordine<br>a condizioni convenienti",
+
+"lt65": "-50% solo oggi!",
+
+"lt67": "Prezzo oggi",
+
+"lt69": "Prezzo standard",
+
+"lt70": "fino alla fine dell’offerta rimangono:",
+
+"lt71": "COMPILATE IL MODULO E<br>CLICCATE «EFFETTUARE L’ORDINE»",
+
+"lt72": "Nazione",
+
+"lt73": "Nome e cognome",
+
+"lt74": "Telefono",
+
+"lt75": "Durata dell’offerta dal 16 febbraio al 18 marzo 2016",
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
